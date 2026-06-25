48f6814109aed6d89e36bd510d82e99c13b565ba
weeeeeeeeeeeeee <rishirungta7@gmail.com>
2026-06-26 04:19:03 +0800

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-4

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-4.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+unordered_map<int,int> mp;
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder,int i , int j , int k , int l)
+    {
+        if(i>j || k>l)
+        return nullptr;
+        // if(j==i || k==l)
+        // return new TreeNode(inorder[i]);
+        else
+        {
+            TreeNode* x= new TreeNode(preorder[k]);
+            int m = mp[x->val];
+            cout<<m<<" ";
+            x->right=build(preorder,inorder,m+1,j,k+m-i+1,l);
+            x->left=build(preorder,inorder,i,m-1,k+1,k+m-i);
+            return x;
+        }
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        // if(preoder.size()==0)
+        // return nullptr;
+        TreeNode* x= new TreeNode(preorder[0]);
+        for(int j=0;j<inorder.size();j++)
+        {
+            mp[inorder[j]]=j;
+        }
+        if(preorder.size()==1)
+        return x;
+        int i= mp[preorder[0]];
+        x->left=build(preorder,inorder,0,i-1,1,i);
+        x->right=build(preorder,inorder,i+1,inorder.size()-1,i+1,inorder.size()-1);
+        return x;
+    }
+};
